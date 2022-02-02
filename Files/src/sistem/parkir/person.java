8514fb5fb367f766b338fa4f17e09aec45a8b5e4
Crypt0n <68642877+Crypt0n13@users.noreply.github.com>
2022-02-02 20:23:30 +0700

    Add files via upload

---
+++ Files/src/sistem/parkir/person.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistem.parkir;
+
+/**
+ *
+ * @author Crypt0n
+ */
+class person {
+    String name;
+    
+    public String method(String name) {
+        this.name = name;
+        return this.name;
+    }
+}
+
